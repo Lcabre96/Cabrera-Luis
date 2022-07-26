336d45d37b707c9555dd1f21a2a4c74bdcf96c92
Luis Cabrera <lcabre.1596@gmail.com>
2022-07-26 18:38:52 -0300

    JS3-Cabrera-Luis

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,4 +1,4 @@
-/* class Placas {
+class Placas {
     constructor(producto, precio, stock) {
         this.producto = producto;
         this.precio = precio;
@@ -9,21 +9,23 @@
 const placaA = new Placas ("RTX 2060", 20000, 20);
 const placaB = new Placas ("RTX 2080", 26000, 10);
 const placaC = new Placas ("RTX 3060", 35000, 15); 
- */
+const placaD = new Placas ("RTX 1660", 12000, 5);
+const placaE = new Placas ("RTX 1080", 22000, 9); 
 
-let placaA = "Rtx 2060"
-let placaB = "Rtx 2080"
-let placaC = "Rtx 3060"
+let listaProductos = [placaA, placaB, placaC, placaD, placaE];
 
-let precioA = 20000;
-let precioB = 26000;
-let precioC = 35000;
+let nombreProductos = [];
 
-let stockA = 20;
-let stockB = 10;
-let stockC = 15;
+function listasProductos(){
+    for(const producto of listaProductos){
+        nombreProductos.push(producto.producto);
+    }
+}
+listasProductos();
 
-let productos = parseInt(prompt("Ingresá cuántos productos queres comprar: \n- Rtx 2060\n- Rtx 2080\n- Rtx 3060"));
+
+let productos = parseInt(prompt("Ingresá cuántos productos querés comprar: \n" + nombreProductos.join("\n")));
+console.log(productos);
 
 let precioTotal = 0;
 
@@ -31,29 +33,36 @@ function calculoPrecio(cantidad, precio) {
     precioTotal += cantidad * precio;
 }
 
-function calculoStock(cantidad, stock, precio) {
-    if (stock >= cantidad) {
-        calculoPrecio(cantidad, precio);
-        alert("El precio total es de: $" +(cantidad * precio));
+function calculoStock(cantidad, producto)  {
+    if (producto.stock >= cantidad) {
+        calculoPrecio(cantidad, producto.precio);
+        alert("El precio total es de: $" +(cantidad * producto.precio));
     }
     else {
         alert("No tenemos esa cantidad en stock. Nuestro stock es de " + stock + " unidades.");
     }
 }
 
+
 for (let i = 0; i < productos; i++) {
 
-    let compra1 = prompt("Ingresá la placa que querías comprar: \n- Rtx 2060\n- Rtx 2080\n- Rtx 3060");
-    let cantidadCompra = parseInt(prompt("Ingresá cuántas unidades querias comprar:"));
+    let compra1 = prompt("Ingresá la placa que querías comprar: \n" + nombreProductos.join("\n")).toLowerCase();
+    let cantidadCompra = parseInt(prompt("Ingresá cuántas unidades querías comprar:"));
 
-    if (compra1 == "Rtx 2060") {
-        calculoStock(cantidadCompra, stockA, precioA);
+    if (compra1 == "rtx 2060") {
+        calculoStock(cantidadCompra, placaA);
     }
-    else if (compra1 == "Rtx 2080") {
-        calculoStock(cantidadCompra, stockB, precioB);
+    else if (compra1 == "rtx 2080") {
+        calculoStock(cantidadCompra, placaB);
     }
-    else if (compra1 == "Rtx 3060") {
-        calculoStock(cantidadCompra, stockC, precioC);
+    else if (compra1 == "rtx 3060") {
+        calculoStock(cantidadCompra, placaC);
+    }
+    else if (compra1 == "rtx 1660") {
+        calculoStock(cantidadCompra, placaD);
+    }
+    else if (compra1 == "rtx 1080") {
+        calculoStock(cantidadCompra, placaE);
     }
     else {
         alert("No tenemos ese producto");
@@ -63,7 +72,6 @@ for (let i = 0; i < productos; i++) {
 if (productos > 1) {
     alert("El precio final de su compra es de $: " + precioTotal);
 }
-
 
 
 
