eacc40378f5816544b538f3d5c6d19902254b7b8
ignacioborraz <ignacioborraz@hotmail.com>
2022-07-27 00:24:26 -0300

    correcciones 3

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,4 +1,4 @@
-class Placas {
+class Placas { //convienen nombres en singular ya que cada vez que se usa: es un contructor "de una unidad"
     constructor(producto, precio, stock) {
         this.producto = producto;
         this.precio = precio;
@@ -6,10 +6,10 @@ class Placas {
     }  
 }
 
-const placaA = new Placas ("RTX 2060", 20000, 20);
-const placaB = new Placas ("RTX 2080", 26000, 10);
-const placaC = new Placas ("RTX 3060", 35000, 15); 
-const placaD = new Placas ("RTX 1660", 12000, 5);
+const placaA = new Placas ("RTX 2060", 20000, 20); //aca creas una
+const placaB = new Placas ("RTX 2080", 26000, 10); //aca otra
+const placaC = new Placas ("RTX 3060", 35000, 15);  //etc
+const placaD = new Placas ("RTX 1660", 12000, 5); //NO TE LA COMPLIQUES CON LOS NOMBRES (Y EN LO POSIBLE SIN ESPACIOS)
 const placaE = new Placas ("RTX 1080", 22000, 9); 
 
 let listaProductos = [placaA, placaB, placaC, placaD, placaE];
@@ -23,8 +23,10 @@ function listasProductos(){
 }
 listasProductos();
 
-
 let productos = parseInt(prompt("Ingresá cuántos productos querés comprar: \n" + nombreProductos.join("\n")));
+//SI TIENE QUE INGRESAR UN NUMERO: NO MOSTRARIA TODOS LOS PRODUCTOS, ES UNA RARA UX
+//DARIA UN MENSAJE DE BIENVENIDA Y PREGUNTARIA LA CANTIDAD NOMAS
+//QUE SUCEDE SI NO ES UN NUMERO???
 console.log(productos);
 
 let precioTotal = 0;
@@ -48,20 +50,24 @@ for (let i = 0; i < productos; i++) {
 
     let compra1 = prompt("Ingresá la placa que querías comprar: \n" + nombreProductos.join("\n")).toLowerCase();
     let cantidadCompra = parseInt(prompt("Ingresá cuántas unidades querías comprar:"));
+    //ARMÁ EL CONDICIONAL NECESARIO PARA QUE:
+    //SI NO EXISTE EL PRODUCTO: no me pregunte las unidades que quiero comprar
 
-    if (compra1 == "rtx 2060") {
+    if (compra1 == "rtx 2060") { //ESTO
         calculoStock(cantidadCompra, placaA);
     }
-    else if (compra1 == "rtx 2080") {
+    else if (compra1 == "rtx 2080") { //ES CASI IGUAL A ESTO
         calculoStock(cantidadCompra, placaB);
     }
-    else if (compra1 == "rtx 3060") {
+    else if (compra1 == "rtx 3060") { //Y ASI...
         calculoStock(cantidadCompra, placaC);
     }
-    else if (compra1 == "rtx 1660") {
+    else if (compra1 == "rtx 1660") { //QUE USAMOS PARA NO REPETIR CODIGO MUUUY PARECIDO??? 
         calculoStock(cantidadCompra, placaD);
     }
-    else if (compra1 == "rtx 1080") {
+    else if (compra1 == "rtx 1080") { //por otro lado: que pasa si el usuario escribe rTx 1080 o RTX 1080 o rtX1080??
+        //por eso los nombres deben ser sencillos
+        //y ademas deben usarse los metodos de comparacion adecuados como toLowerCase()
         calculoStock(cantidadCompra, placaE);
     }
     else {
@@ -69,7 +75,7 @@ for (let i = 0; i < productos; i++) {
     }
 }
 
-if (productos > 1) {
+if (productos > 1) { //y si es 1??? porque si compro un solo producto no me dice el total???
     alert("El precio final de su compra es de $: " + precioTotal);
 }
 
