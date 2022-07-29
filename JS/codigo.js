e71dc51dc82cde48c8cdad22388b059547d819c9
Luis Cabrera <lcabre.1596@gmail.com>
2022-07-28 21:21:58 -0300

    JS4-Cabrera-Luis

--- JS/codigo.js
+++ JS/codigo.js
@@ -1,4 +1,4 @@
-class Placas { //convienen nombres en singular ya que cada vez que se usa: es un contructor "de una unidad"
+class Placa {
     constructor(producto, precio, stock) {
         this.producto = producto;
         this.precio = precio;
@@ -6,28 +6,23 @@ class Placas { //convienen nombres en singular ya que cada vez que se usa: es un
     }  
 }
 
-const placaA = new Placas ("RTX 2060", 20000, 20); //aca creas una
-const placaB = new Placas ("RTX 2080", 26000, 10); //aca otra
-const placaC = new Placas ("RTX 3060", 35000, 15);  //etc
-const placaD = new Placas ("RTX 1660", 12000, 5); //NO TE LA COMPLIQUES CON LOS NOMBRES (Y EN LO POSIBLE SIN ESPACIOS)
-const placaE = new Placas ("RTX 1080", 22000, 9); 
+const placaA = new Placa ("rtx2060", 20000, 20);
+const placaB = new Placa ("rtx2080", 26000, 10);
+const placaC = new Placa ("rtx3060", 35000, 15); 
+const placaD = new Placa ("rtx1660", 12000, 5);
 
-let listaProductos = [placaA, placaB, placaC, placaD, placaE];
 
-let nombreProductos = [];
+let listaProductos = [placaA, placaB, placaC, placaD];
 
-function listasProductos(){
-    for(const producto of listaProductos){
-        nombreProductos.push(producto.producto);
-    }
+let nombreProductos = listaProductos.map((placas) => placas.producto);
+
+let productos = parseInt(prompt("¡Bienvenido!, ingresá cuantos productos quieres comprar:"));
+
+
+while(isNaN(productos)){
+    alert("No ingresaste un número, Por favor intentá de nuevo.")
+    productos = parseInt(prompt("¡Bienvenido!, ingresá cuantos productos quieres comprar:"));
 }
-listasProductos();
-
-let productos = parseInt(prompt("Ingresá cuántos productos querés comprar: \n" + nombreProductos.join("\n")));
-//SI TIENE QUE INGRESAR UN NUMERO: NO MOSTRARIA TODOS LOS PRODUCTOS, ES UNA RARA UX
-//DARIA UN MENSAJE DE BIENVENIDA Y PREGUNTARIA LA CANTIDAD NOMAS
-//QUE SUCEDE SI NO ES UN NUMERO???
-console.log(productos);
 
 let precioTotal = 0;
 
@@ -47,41 +42,28 @@ function calculoStock(cantidad, producto)  {
 
 
 for (let i = 0; i < productos; i++) {
-
+    
     let compra1 = prompt("Ingresá la placa que querías comprar: \n" + nombreProductos.join("\n")).toLowerCase();
-    let cantidadCompra = parseInt(prompt("Ingresá cuántas unidades querías comprar:"));
-    //ARMÁ EL CONDICIONAL NECESARIO PARA QUE:
-    //SI NO EXISTE EL PRODUCTO: no me pregunte las unidades que quiero comprar
+    let cantidadCompra= parseInt(prompt("¿Cuántas unidades querés?"));
+    
 
-    if (compra1 == "rtx 2060") { //ESTO
+    if (compra1 == "rtx2060") {
         calculoStock(cantidadCompra, placaA);
     }
-    else if (compra1 == "rtx 2080") { //ES CASI IGUAL A ESTO
+    else if (compra1 == "rtx2080") {
         calculoStock(cantidadCompra, placaB);
     }
-    else if (compra1 == "rtx 3060") { //Y ASI...
+    else if (compra1 == "rtx3060") {
         calculoStock(cantidadCompra, placaC);
     }
-    else if (compra1 == "rtx 1660") { //QUE USAMOS PARA NO REPETIR CODIGO MUUUY PARECIDO??? 
+    else if (compra1 == "rtx1660") {
         calculoStock(cantidadCompra, placaD);
-    }
-    else if (compra1 == "rtx 1080") { //por otro lado: que pasa si el usuario escribe rTx 1080 o RTX 1080 o rtX1080??
-        //por eso los nombres deben ser sencillos
-        //y ademas deben usarse los metodos de comparacion adecuados como toLowerCase()
-        calculoStock(cantidadCompra, placaE);
     }
     else {
         alert("No tenemos ese producto");
     }
 }
 
-if (productos > 1) { //y si es 1??? porque si compro un solo producto no me dice el total???
+if (productos >= 1) {
     alert("El precio final de su compra es de $: " + precioTotal);
 }
-
-
-
-
-
-
-
